73762b26dca7d3b6e9b99d065459bd34b717a13f
Rich Robinson <robinsonrc@gmail.com>
2014-08-24 09:43:33 +0100

    Add doc comments

--- RCRTableViewRefreshController/RCRTableViewRefreshController.h
+++ RCRTableViewRefreshController/RCRTableViewRefreshController.h
@@ -7,12 +7,24 @@
 
 #import <UIKit/UIKit.h>
 
-/// This is a non-ideal way of adding a refresh control to a table view. The proper, correct way to do it is by using a UITableViewController class rather than working with a UITableView directly. However, this is not always practical -particularly when adding a refresh control to an already existing table view, with an existing class hierarchy, and working to a time limit!
+/**
+ A simple workaround for using the standard iOS @c UIRefreshControl with a regular @c UITableView without needing a @c UITableViewController.
 
+ For the record, this is a non-ideal way of adding a refresh control to a table view. The proper, correct way to do it is by using a @c UITableViewController class rather than working with a @c UITableView directly. However, this is not always practical - particularly when adding a refresh control to an existing table view, with an existing class hierarchy.
+ */
 @interface RCRTableViewRefreshController : NSObject
 
+/**
+ Designated initializer.
+ 
+ @param tableView The @c UITableView to be augmented with a <tt>UIRefreshControl</tt>.
+ @param refreshHandler A block to be executed when the refresh control is activated.
+ */
 - (instancetype)initWithTableView:(UITableView *)tableView refreshHandler:(void (^) ())refreshHandler NS_DESIGNATED_INITIALIZER;
 
+/**
+ Tell the refresh control that the refresh operation has ended.
+ */
 - (void)endRefreshing;
 
 @end
