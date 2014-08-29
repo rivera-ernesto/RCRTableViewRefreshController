530edb2ae7666f35f1b61163249d787f920056fd
Rich Robinson <robinsonrc@gmail.com>
2014-08-29 07:05:05 +0100

    Update documentation comments to work better with CocoaDocs (whilst still working with Xcode)

--- RCRTableViewRefreshController/RCRTableViewRefreshController.h
+++ RCRTableViewRefreshController/RCRTableViewRefreshController.h
@@ -8,16 +8,16 @@
 #import <UIKit/UIKit.h>
 
 /**
- A simple workaround for using the standard iOS @c UIRefreshControl with a regular @c UITableView without needing a @c UITableViewController.
+ A simple workaround for using the standard iOS <tt>UIRefreshControl</tt> with a regular <tt>UITableView</tt> without needing a <tt>UITableViewController</tt>.
 
- For the record, this is a non-ideal way of adding a refresh control to a table view. The proper, correct way to do it is by using a @c UITableViewController class rather than working with a @c UITableView directly. However, this is not always practical - particularly when adding a refresh control to an existing table view, with an existing class hierarchy.
+ For the record, this is a non-ideal way of adding a refresh control to a table view. The proper, correct way to do it is by using a <tt>UITableViewController</tt> class rather than working with a <tt>UITableView</tt> directly. However, this is not always practical - particularly when adding a refresh control to an existing table view, with an existing class hierarchy.
  */
 @interface RCRTableViewRefreshController : NSObject
 
 /**
  Designated initializer.
  
- @param tableView The @c UITableView to be augmented with a <tt>UIRefreshControl</tt>.
+ @param tableView The <tt>UITableView</tt> to be augmented with a <tt>UIRefreshControl</tt>.
  @param refreshHandler A block to be executed when the refresh control is activated.
  */
 - (instancetype)initWithTableView:(UITableView *)tableView refreshHandler:(void (^) ())refreshHandler /*NS_DESIGNATED_INITIALIZER*/;
