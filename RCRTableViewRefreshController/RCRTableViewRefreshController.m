d8a9c7402110658ce886bb66712b1602e8976169
Rich Robinson <robinsonrc@gmail.com>
2014-08-29 08:48:43 +0100

    Add more detailed comments

--- RCRTableViewRefreshController/RCRTableViewRefreshController.m
+++ RCRTableViewRefreshController/RCRTableViewRefreshController.m
@@ -24,10 +24,13 @@
     if (self) {
         _refreshHandler = refreshHandler;
         
-        // Create a UITableViewController for our internal use, and set its tableView property to point at the tableView passed into this method
+        // In order to use a UIRefreshControl on a the supplied table view we can use a UITableViewController behind the scenes to wire up the refresh control and table view, and get them working together. The table view controller is never actually presented - it's purely used as a workaround for adding the refresh control to the table view
+        
+        // First, create a UITableViewController for our internal use and set its tableView property to point at the table view passed into this method
         _tableViewController = [[UITableViewController alloc] init];
         _tableViewController.tableView = tableView;
         
+        // Then create our UIRefreshControl, get it to run our refres handler when it's activated, and add it to the table view controller
         _refreshControl = [[UIRefreshControl alloc] init];
         [_refreshControl addTarget:self action:@selector(doRefresh) forControlEvents:UIControlEventValueChanged];
 
--- RCRTableViewRefreshControllerSample/RCRTableViewRefreshControllerSample/ViewController.h
+++ RCRTableViewRefreshControllerSample/RCRTableViewRefreshControllerSample/ViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
+// Note that this view controller is not a subclass of UITableViewController, which you would generally need it to be in order to use a UIRefreshControl with the table view. RCRTableViewRefreshController allows us to work around this and use a regular UIViewcontroller (or any subclass of UIViewcontroller)
 @interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
 
 @end
